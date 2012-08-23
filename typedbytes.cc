c9f1475043b4cccf430572485e564d3494606de8
Austin Benson <arbenson@berkeley.edu>
2012-08-23 12:45:19 -0700

    use sizeof

--- typedbytes.cc
+++ typedbytes.cc
@@ -86,7 +86,7 @@ bool TypedBytesInFile::_read_opaque_primitive(typedbytes_opaque& buffer,
                         &longbuf, sizeof(int64_t), 1, stream));
                     push_opaque_bytes(buffer, 
                         (unsigned char*)&longbuf, sizeof(int64_t));
-                    len -= 8;
+                    len -= sizeof(int64_t);
                 } else {
                     RETURNIFFALSE(checkedfread(&longbuf, len, 1, stream));
                     push_opaque_bytes(buffer, 
